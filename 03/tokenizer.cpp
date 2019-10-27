c2707ee58baa3fecad2ad9fa9ee9ae49f92046d5
Stanislav Yarkin <yarkinsv@gmail.com>
2019-10-27 19:46:55 +0300

    review fixes

--- 03/tokenizer.cpp
+++ 03/tokenizer.cpp
@@ -2,24 +2,16 @@
 #include "tokenizer.hpp"
 
 
-static bool isNumber(const std::string& token) {
-    bool check = true;
-    try {
-        stol(token);
-    } catch (const std::invalid_argument& e) {
-        check = false;
-    } catch (const std::out_of_range& e) {
-        check = false;
+bool isNumber(const std::string& token) {
+    if (token.length() > 10 || token.length() == 0) {
+        return false;
     }
-    return check;
-}
-
-static bool isNotNumber(const std::string& token) {
-    return !isNumber(token);
-}
-
-static bool isDelimiter(const char& c) {
-    return c == ' ' || c == '\t' || c == '\n';
+    for (char c : token) {
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+    return !(token.length() == 10 && token > "2147483647");
 }
 
 void Tokenizer::parse(const std::string& str) {
@@ -28,18 +20,19 @@ void Tokenizer::parse(const std::string& str) {
     }
 
     for (unsigned long start = 0; start < str.length(); start++) {
-        while (start < str.length() && isDelimiter(str[start])) {
+        while (start < str.length() && isspace(str[start])) {
             start++;
         }
         unsigned long end = start;
-        while (end < str.length() && !isDelimiter(str[end])) {
+        while (end < str.length() && !isspace(str[end])) {
             end++;
         }
         if (start != end) {
             std::string token = str.substr(start, end - start);
             for (const auto& onParseTokenCallback : onParseTokenCallbacks) {
-                auto predicate = onParseTokenCallback.first;
-                auto callback = onParseTokenCallback.second;
+                TokenPredicate predicate;
+                OnTokenParsed callback;
+                std::tie(predicate, callback) = onParseTokenCallback;
                 if (predicate(token)) {
                     callback(token);
                 }
@@ -51,28 +44,4 @@ void Tokenizer::parse(const std::string& str) {
     for (const auto& onFinishCallback : onFinishCallbacks) {
         onFinishCallback();
     }
-}
-
-void Tokenizer::addOnParseNumber(const OnTokenParsed& callback) {
-    addOnParseToken(isNumber, callback);
-}
-
-void Tokenizer::addOnParseNumber(const OnNumberParsed& callback) {
-    addOnParseToken(isNumber, [&callback](const std::string& token) { callback(stol(token)); });
-}
-
-void Tokenizer::addOnParseText(const OnTokenParsed& callback) {
-    addOnParseToken(isNotNumber, callback);
-}
-
-void Tokenizer::addOnParseToken(const TokenPredicate& predicate, const OnTokenParsed& callback) {
-    onParseTokenCallbacks.emplace_back(predicate, callback);
-}
-
-void Tokenizer::addOnStart(const OnStart& callback) {
-    onStartCallbacks.push_back(callback);
-}
-
-void Tokenizer::addOnFinish(const OnFinish& callback) {
-    onFinishCallbacks.push_back(callback);
 }
--- 03/tokenizer.hpp
+++ 03/tokenizer.hpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <cstddef>
 #include <functional>
 #include <vector>
@@ -7,6 +8,8 @@ using OnTokenParsed = std::function<void (const std::string& token)>;
 using OnNumberParsed = std::function<void (const long token)>;
 using OnStart = std::function<void ()>;
 using OnFinish = std::function<void ()>;
+
+bool isNumber(const std::string& token);
 
 // Токенайзер, которому можно зарегистрировать набор callback'ов на разные события.
 // На каждое событие можно зарегистрировать ниодного либо сразу множество callback'ов,
@@ -18,16 +21,28 @@ public:
     // Спарсить строку на токены
     void parse(const std::string& str);
     // Зарегистрировать callback, когда спарсилось число, пусть будет возможность получить в виде текста, либо в виде long'а
-    void addOnParseNumber(const OnTokenParsed& callback);
-    void addOnParseNumber(const OnNumberParsed& callback);
+    inline void addOnParseNumber(const OnTokenParsed& callback) {
+        addOnParseToken(isNumber, callback);
+    }
+    inline void addOnParseNumber(const std::function<void (const long token)>& callback) {
+        addOnParseToken(isNumber, [&callback](const std::string& token) { callback(stol(token)); });
+    }
     // Зарегистрировать callback, если спарсилось не число, или слишком длинное число чтобы поместиться в long
-    void addOnParseText(const OnTokenParsed& callback);
+    inline void addOnParseText(const OnTokenParsed& callback) {
+        addOnParseToken(std::not_fn(isNumber), callback);
+    }
     // Возможность загегистрировать более универсальные callback'и
-    void addOnParseToken(const TokenPredicate& predicate, const OnTokenParsed& callback);
+    inline void addOnParseToken(const TokenPredicate& predicate, const OnTokenParsed& callback) {
+        onParseTokenCallbacks.emplace_back(predicate, callback);
+    }
     // Зарегистрировать callback, который вызовется перед началом парсинга
-    void addOnStart(const OnStart& callback);
+    inline void addOnStart(const OnStart& callback) {
+        onStartCallbacks.push_back(callback);
+    }
     // Зарегистрировать callback, который вызовется по завершение парсинга
-    void addOnFinish(const OnFinish& callback);
+    inline void addOnFinish(const OnFinish& callback) {
+        onFinishCallbacks.push_back(callback);
+    }
 
 private:
     std::vector<std::pair<TokenPredicate, OnTokenParsed>> onParseTokenCallbacks;
