53d90e0d989bab7d4770f5543b34e277967eef7b
Stas Yarkin <yarkinsv@gmail.com>
2019-11-22 15:15:10 +0300

    HW03 (#3)
    * HW03
    
    * review fixes

---
+++ 06/README.md
@@ -0,0 +1,6 @@
+## homework_03:
+```
+git clone git@github.com:yarkinsv/made_2019_cpp.git
+cd made_2019_cpp/03
+make
+```
---
+++ algo/03/task_7_3/main.cpp
@@ -0,0 +1,210 @@
+// 7_3. Порядковые статистики ДД
+//Множество натуральных чисел постоянно меняется. Элементы в нем добавляются и удаляются по одному.
+// Вычислите указанные порядковые статистики после каждого добавления или удаления.
+//Требуемая скорость выполнения запроса - O(log n) в среднем.
+//В реализации используйте декартовы деревья.
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+
+struct Node {
+    int key; // Значение
+    int priority; // Приоритет
+    Node* right;
+    Node* left;
+    int size; // Размер дерева, 1 - если дерево из одной вершины
+};
+
+void destroy_tree(Node* node) {
+    if (node != nullptr) {
+        destroy_tree(node->left);
+        destroy_tree(node->right);
+        delete node;
+    }
+}
+
+class Treap {
+public:
+    // Обход графа в клубину, на каждый посещенный узел вызов функции callback
+    void pre_order(const std::function<void (Node&)>& callback);
+
+    // Разрезание декартового дерева по ключу.
+    // В процессе отделения дерева, нужно обновить у того дерева, из которого идет удаление, размер
+    static void split(Node* node, int key, Node*& left, Node*& right);
+
+    // Слияние двух декартовых деревьев.
+    // При слиянии мы увеличиваем размер в ветке дерева, к которой идет добавление поддерева
+    static Node* merge(Node* left, Node* right);
+
+    // Вставка по ключу и приоритету
+    void insert(int key, int priority);
+
+    // Удаление по ключу
+    void remove(int key);
+
+    // Нахождение k-ой статистики в дереве
+    Node* statistic(int k);
+
+    void print() {
+        pre_order([] (Node& node) { std::cout << "key: " << node.key << " size: " << node.size <<  " priority: " << node.priority
+        << " left: " << (node.left ? node.left->key : -1) << " right: " << (node.right ? node.right->key : -1) << "\n"; });
+    }
+
+    ~Treap() {
+        destroy_tree(this->root);
+    }
+
+private:
+    Node* root = nullptr;
+};
+
+
+void Treap::pre_order(const std::function<void (Node&)>& callback) {
+    // Для обхода в глубину используется очередь
+    std::vector<Node*> queue;
+    queue.push_back(root);
+    while (!queue.empty()) {
+        Node* node = queue.back();
+        queue.pop_back();
+        callback(*node);
+        if (node->right != nullptr) {
+            queue.push_back(node->right);
+        }
+        if (node->left != nullptr) {
+            queue.push_back(node->left);
+        }
+    }
+}
+
+// Разрезание декартового дерева по ключу.
+// В процессе отделения дерева, нужно обновить у того дерева, из которого идет удаление, размер
+void Treap::split(Node* node, int key, Node*& left, Node*& right) {
+    if (node == nullptr) {
+        left = right = nullptr;
+    } else if (node->key <= key) {
+        split(node->right, key, node->right, right);
+        left = node;
+        if (right != nullptr) {
+            left->size -= right->size;
+        }
+    } else {
+        split(node->left, key, left, node->left);
+        right = node;
+        if (left != nullptr) {
+            right->size -= left->size;
+        }
+    }
+}
+
+// Слияние двух декартовых деревьев.
+// При слиянии мы увеличиваем размер в ветке дерева, к которой идет добавление поддерева
+Node* Treap::merge(Node* left, Node* right) {
+    if (!left || !right)
+        return left == nullptr ? right : left;
+    if (left->priority > right->priority) {
+        left->size += right->size;
+        left->right = merge(left->right, right);
+        return left;
+    } else {
+        right->size += left->size;
+        right->left = merge(left, right->left);
+        return right;
+    }
+}
+
+// Вставка по ключу и приоритету
+void Treap::insert(int key, int priority) {
+    Node* newNode = new Node {key, priority, nullptr, nullptr, 1};
+    if (root == nullptr) {
+        root = newNode;
+        return;
+    }
+    Node* t1 = nullptr;
+    Node* t2 = nullptr;
+    split(root, key, t1, t2);
+    t1 = merge(t1, newNode);
+    root = merge(t1, t2);
+}
+
+// Удаление по ключу
+void Treap::remove(int key) {
+    Node* node = root;
+    Node* parent = nullptr;
+    bool found = false;
+
+    // Бинарный поиск ноды с искомым ключом
+    while (node != nullptr && !found) {
+        node->size--;
+        if (key < node->key) {
+            parent = node;
+            node = node->left;
+        } else if (key > node->key) {
+            parent = node;
+            node = node->right;
+        } else {
+            found = true;
+        }
+    }
+
+    if (!found) {
+        throw std::invalid_argument("Key value not found in tree");
+    }
+
+    // Сливаем поддеревья удаляемой ноды и восстанавливаем связи
+    Node* merged = merge(node->left, node->right);
+    if (parent == nullptr) {
+        root = merged;
+    } else if (parent->left != nullptr && parent->left->key == key) {
+        parent->left = merged;
+    } else {
+        parent->right = merged;
+    }
+
+    // Удаленная нода больше не нужна, можно очистить ее из памяти
+    delete node;
+}
+
+// Нахождение k-ой статистики в дереве
+Node* Treap::statistic(int k) {
+    Node* node = root;
+    // Статистику ноды можно определить исходя из размера поддерева текущей ноды
+    // Для root'а ранк определить легко по размеру левого дерева, т.к. все элементы в нем будут меньше чем он
+    int curK = node->left ? node->left->size : 0;
+    while (curK != k) {
+        if (curK > k) {
+            node = node->left;
+            // Если идем в левое поддерево, то ранк ноды нужно уменьшить на размер его правого дерева
+            curK = curK - 1 - (node->right ? node->right->size : 0);
+        } else {
+            node = node->right;
+            // Если идем в правое поддерево, то ранк ноды нужно увеличить на размер его левого дерева
+            curK = curK + 1 + (node->left ? node->left->size : 0);
+        }
+    }
+    return node;
+}
+
+
+
+int main() {
+    Treap tree;
+
+    int n = 0;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int value = 0;
+        int k = 0;
+        std::cin >> value >> k;
+        if (value > 0) {
+            tree.insert(value, std::rand());
+        } else {
+            tree.remove(-value);
+        }
+        std::cout << tree.statistic(k)->key << " ";
+    }
+
+    return 0;
+}
