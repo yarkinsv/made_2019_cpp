ed23598098e981b0bc2a560cfe3fee12a0bccc0b
Stanislav Yarkin <yarkinsv@gmail.com>
2019-12-13 16:07:48 +0300

    HW_VECTOR

--- 08/test.cpp
+++ 08/test.cpp
@@ -8,16 +8,45 @@
 int tests_run = 0;
 
 static const char* test1() {
+    Vector v = { 1, 2, 3 };
+    v[1] = 10;
+    mu_assert("v[0] == 1", v[0] == 1);
+    mu_assert("v[1] == 2", v[1] == 10);
+    mu_assert("v[2] == 3", v[2] == 3);
+    return nullptr;
+}
+
+static const char* test2() {
+    Vector data1 = { 1, 6, 1, 3, 4, 5, 6 };
+    int sum = 0;
+    std::for_each(data1.begin(), data1.end(), [&sum](int x) { sum += x; });
+    mu_assert("sum == 26", sum == 26);
+
+    std::set<int> data2(data1.begin(), data1.end());
+    sum = 0;
+    std::for_each(data2.begin(), data2.end(), [&sum](int x) { sum += x; });
+    mu_assert("sum == 26", sum == 26);
 
     return nullptr;
 }
 
-
+static const char* test3() {
+    Vector<float> v(1);
+    v.push_back(1.1);
+    v.push_back(10.0);
+    v.push_back(3);
+    mu_assert("v[0] == 1", v[0] == 1.1);
+    mu_assert("v[1] == 2", v[1] == 10);
+    mu_assert("v[2] == 3", v[2] == 3);
+    mu_assert("v.size() == 3", v.size() == 3);
+    return nullptr;
+}
 
 
 static const char* all_tests() {
     mu_run_test(test1);
-
+    mu_run_test(test2);
+    mu_run_test(test3);
     return nullptr;
 }
  
